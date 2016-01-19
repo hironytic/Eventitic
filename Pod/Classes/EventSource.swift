9d9d6623280430a4bf7c4d2b1b097677da73da69
Hironori Ichimiya <hiron@hironytic.com>
2016-01-19 23:02:27 +0900

    Add classes and test cases

---
+++ Pod/Classes/EventSource.swift
@@ -0,0 +1,70 @@
+//
+// EventSource.swift
+// Eventitic
+//
+// Copyright (c) 2016 Hironori Ichimiya <hiron@hironytic.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import Foundation
+
+private class UnlistenPool<T> {
+    private var listeners: [Listener<T>] = []
+}
+
+public class EventSource<T> {
+    private var listeners: [Listener<T>]
+    private var unlistenPools: [UnlistenPool<T>]
+
+    public init() {
+        listeners = []
+        unlistenPools = []
+    }
+    
+    public func listen(handler: T -> Void) -> Listener<T> {
+        let listener = Listener(eventSource: self, handler: handler)
+        listeners.append(listener)
+        return listener
+    }
+    
+    func unlisten(listener: Listener<T>) {
+        if let pool = unlistenPools.last {
+            pool.listeners.append(listener)
+        } else {
+            if let index = listeners.indexOf(listener) {
+                listeners.removeAtIndex(index)
+            }
+        }
+    }
+    
+    public func fire(value: T) {
+        unlistenPools.append(UnlistenPool<T>())
+        
+        listeners.forEach { listener in
+            listener.handleEvent(value)
+        }
+        
+        let pool = unlistenPools.removeLast()
+        
+        if pool.listeners.count > 0 {
+            listeners = listeners.filter { !pool.listeners.contains($0) }
+        }
+    }
+}
---
+++ Pod/Classes/Listener.swift
@@ -0,0 +1,58 @@
+//
+// Listener.swift
+// Eventitic
+//
+// Copyright (c) 2016 Hironori Ichimiya <hiron@hironytic.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import Foundation
+
+public protocol Unlistenable {
+    func unlisten()
+}
+
+public class Listener<T>: Unlistenable, Equatable {
+    public typealias Handler = T -> Void
+
+    private let eventSource: EventSource<T>
+    private let handler: Handler
+ 
+    public init(eventSource: EventSource<T>, handler: Handler) {
+        self.eventSource = eventSource
+        self.handler = handler;
+    }
+    
+    public func handleEvent(value: T) {
+        handler(value)
+    }
+    
+    public func unlisten() {
+        eventSource.unlisten(self)
+    }
+    
+    public func addToStore(listenerStore: ListenerStore) {
+        listenerStore.add(self)
+    }
+}
+
+public func ==<T>(lhs: Listener<T>, rhs: Listener<T>) -> Bool {
+    return lhs === rhs
+}
